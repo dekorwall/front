fd6ecc758f5b63a41e084792c95942a234e18856
Roman Yadushinskiy <roman@yad.dev>
2023-06-12 17:23:36 +0300

    Add footer

--- src/modules/menu/components/menu-item/menu-item.component.tsx
+++ src/modules/menu/components/menu-item/menu-item.component.tsx
@@ -1,38 +1,74 @@
+// import { FC } from "react";
+
+// interface MenuItemProps {
+//   imagePath: string;
+//   weight: number;
+//   title: string;
+//   description: string;
+//   price: number;
+// };
+
+// export const MenuItem: FC<MenuItemProps> = ({
+//     imagePath,
+//     weight,
+//     title,
+//     description,
+//     price,
+// }) => {
+//     return (
+//         <div className="w-96 shadow-xl rounded-2xl">
+//             <div className="relative mb-8">
+//                 <img
+//                     className="object-cover object-center w-full h-[15rem] round-t-2xl"
+//                     src={imagePath}
+//                     alt={title}
+//                 />
+//                 <span className="absolute bottom-1.5 right-3 bg-gray-900/50 text-white text-sm px-2 rounded-[2rem]">
+//                     {weight}л
+//                 </span>
+//             </div>
+//             <div className="p-8">
+//                 <h2 className="text-xl font-semibold mb-2">{title}</h2>
+//                 <p className="mb-8">
+//                     {description}
+//                 </p>
+//                 <span className="text-xl font-semibold">{price}грн</span>
+//             </div>
+//         </div>
+//     )
+// };
+
 import { FC } from "react";
 
 interface MenuItemProps {
-  imagePath: string;
-  weight: number;
-  title: string;
-  description: string;
-  price: number;
+    imagePath: string;
+    weight: number;
+    title: string;
+    description: string;
+    price: number;
 };
 
-export const MenuItem: FC<MenuItemProps> = ({
-    imagePath,
-    weight,
-    title,
-    description,
-    price,
-}) => {
+export const MenuItem: FC<MenuItemProps> = ({ imagePath, weight, title, description, price }) => {
     return (
-        <div className="w-96 shadow-xl rounded-2xl">
-            <div className="relative mb-8">
+        <div className="w-96 shadow-xl rounded-2xl bg-white">
+            <div className="relative">
                 <img
+                    className="object-cover object-center w-full h-[15rem] round-t-2xl"
                     src={imagePath}
-                    alt="dekor"
-                    className="object-cover object-center w-full rounded-t-2xl"
+                    alt={title}
                 />
                 <span className="absolute bottom-1.5 right-3 bg-gray-900/50 text-white text-sm px-2 rounded-[2rem]">
-                    {weight}л
+                    {weight} л
                 </span>
-            </div>
-            <div className="p-8">
-                <h2 className="text-xl font-semibold mb-2">{title}</h2>
-                <p className="mb-8">
-                    {description}
-                </p>
-                <span className="text-xl font-semibold">{price}грн</span>
+                <div className="p-8">
+                    <h2 className="text-xl font-semibold mb-2">
+                        {title}
+                    </h2>
+                    <p className="mb-8">
+                        {description}
+                    </p>
+                    <span className="text-xl font-semibold">{price} грн</span>
+                </div>
             </div>
         </div>
     )

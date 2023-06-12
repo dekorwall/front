69c251cee229e031e933d5fa03144ec8a9b385a1
Roman Yadushinskiy <roman@yad.dev>
2023-06-12 17:51:05 +0300

    Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { Header } from "@app/common/components/header/header.component";
+import { MenuList } from "@app/modules/menu/components/menu-list/menu-list.component";
+import { Footer } from '@app/common/components/footer/footer.component';
+import dekorList from '@app/mocks/dekor.json';
+
 export const App = () => {
   return (
-    <h1 className="font-bold text-red-500">Pizza</h1>
+    <div>
+      <Header />
+      <div className="mx-12 mb-24">
+        <MenuList items={dekorList}/>
+      </div>
+      <Footer/>
+    </div>
   );
 }
 
